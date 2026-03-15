0a1dc113b4de45216c5d95a156afd0876c9ab203
Shun <98299948+PachonCake@users.noreply.github.com>
2026-03-14 20:57:12 -0600

    Update diccionario-data.js

--- diccionario-data.js
+++ diccionario-data.js
@@ -4,32 +4,35 @@ const diccionarioMedico = {
     definicion: "Unidad estructural y funcional básica de los seres vivos.",
     ejemplo: "Las células musculares permiten el movimiento del cuerpo.",
     relacionadas: ["tejido", "organelo", "ADN"]
-  },
-  
+  }, 
   homeostasis: {
     titulo: "Homeostasis",
     definicion: "Capacidad del organismo para mantener condiciones internas estables.",
     ejemplo: "La regulación de la temperatura corporal es un ejemplo de homeostasis.",
     relacionadas: ["equilibrio", "metabolismo"]
   },
-
   taquicardia: {
     titulo: "Taquicardia",
     definicion: "Frecuencia cardíaca mayor a 100 latidos por minuto.",
     ejemplo: "El paciente presentó taquicardia tras el ejercicio intenso.",
     relacionadas: ["arritmia", "corazón"]
   },
-
   mermelada: {
     titulo: "mermelada",
     definicion: "Frecuencia cardíaca mayor a 100 latidos por minuto.",
     ejemplo: "Tu buscando esta palabra.",
     relacionadas: ["procrastinacion", "flojera", "ADHD"]
   },
-
   arteria: {
     titulo: "Arteria",
     definicion: "Vaso sanguíneo que transporta sangre desde el corazón.",
     ejemplo: "M",
     relacionadas: ["vena", "corazón"]
+  },
+  vena: {
+    titulo: "Vena",
+    definicion: "Vaso sanguíneo que transporta sangre al corazón.",
+    ejemplo: "m",
+    relacionadas: ["arritmia", "corazón"]
+  }
 };
