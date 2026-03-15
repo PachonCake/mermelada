73b24451a14b1c7219b288a0942a6dd1bc46a058
Shun <98299948+PachonCake@users.noreply.github.com>
2026-03-14 20:59:30 -0600

    Update diccionario.js

--- diccionario.js
+++ diccionario.js
@@ -1,11 +1,3 @@
-const diccionarioMedico = {
-  celula: "Unidad básica estructural y funcional de los seres vivos.",
-  homeostasis: "Capacidad del organismo para mantener condiciones internas estables.",
-  taquicardia: "Frecuencia cardíaca mayor a 100 latidos por minuto.",
-  arteria: "Vaso sanguíneo que transporta sangre desde el corazón.",
-  mermelada: "Oyeme! ¿Es enserió? Ponte a trabajar y deja de distraerte",
-  vena: "Vaso sanguíneo que lleva sangre al corazón."
-};
 function buscarTermino(){
   let palabra = document.getElementById("busqueda").value.toLowerCase();
   let sugerencias = document.getElementById("sugerencias");
